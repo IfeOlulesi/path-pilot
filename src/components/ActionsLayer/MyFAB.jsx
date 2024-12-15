8f4c27857138fe3e3eb2acbe0e3f320382519648
Ife <ifeolulesi@gmail.com>
2024-12-15 02:39:03 +0100

    feat: Added 4 minor FABs

--- src/components/ActionsLayer/MyFAB.jsx
+++ src/components/ActionsLayer/MyFAB.jsx
@@ -1,13 +1,17 @@
 import PropTypes from 'prop-types';
 
-export default function MyFAB({ icon }) {
+export default function MyFAB({ icon, color, border = false, borderColor = "", onClick }) {
 	return (
-		<div className="bg-red-500/25 w-full h-full">
+		<div className={`bg-[${color}] py-3.5 px-4 rounded-full inline-flex place-content-center shadow-md cursor-pointer ${border && `border-2 border-[${borderColor}]`}`} onClick={onClick}>
 			{icon}
 		</div>
 	);
 }
 
 MyFAB.propTypes = {
-	icon: PropTypes.element
+	icon: PropTypes.element,
+	color: PropTypes.string,
+	border: PropTypes.bool,
+	borderColor: PropTypes.string,
+	onClick: PropTypes.func
 };
--- src/components/ActionsLayer/index.jsx
+++ src/components/ActionsLayer/index.jsx
@@ -1,11 +1,74 @@
-import PathPilotLogoMini from "../../assets/PathPilotLogoMini";
-import MyFAB from '@/components/ActionsLayer/MyFAB'
+import theme from "@/utils/theme";
+import PathPilotLogoMini from "@/assets/PathPilotLogoMini";
+import MyFAB from "@/components/ActionsLayer/MyFAB";
+import { useState } from "react";
+import { BrickWall, Eraser, Flag, MapPin, X } from "lucide-react";
 
 export default function ActionsLayer() {
+	const [showButtons, setShowButtons] = useState(false);
+
+	const handleShowButtons = () => {
+		console.log("showButtons: ", showButtons);
+		setShowButtons(!showButtons);
+	};
+
+	const secActions = [
+		{
+			icon: <BrickWall color={theme.lightMode.primary} size={20} />,
+			color: theme.lightMode.white,
+			border: true,
+			borderColor: theme.lightMode.primary,
+		},
+		{
+			icon: <Eraser color={theme.lightMode.primary} size={20} />,
+			color: theme.lightMode.white,
+			border: true,
+			borderColor: theme.lightMode.primary,
+		},
+		{
+			icon: <MapPin color={theme.lightMode.primary} size={20} />,
+			color: theme.lightMode.white,
+			border: true,
+			borderColor: theme.lightMode.primary,
+		},
+		{
+			icon: <Flag color={theme.lightMode.primary} size={20} />,
+			color: theme.lightMode.white,
+			border: true,
+			borderColor: theme.lightMode.primary,
+		},
+	];
 
 	return (
-		<div className="absolute bottom-0 left-0 bg-red-500/25 w-full h-full">
-			<MyFAB icon={<PathPilotLogoMini color="#101928" />} />
+		<div className="absolute bottom-0 right-0 bg-red-500/0">
+			<div className="absolute bottom-0 right-0 p-4">
+				<div className="flex flex-col gap-2 pb-4">
+					{showButtons &&
+						secActions.map((action, index) => (
+							<MyFAB
+								icon={action.icon}
+								color={action.color}
+								className="absolute bottom-0 right-0"
+								border={action.border}
+								borderColor={action.borderColor}
+								// onClick={handleShowButtons}
+								key={index}
+							/>
+						))}
+				</div>
+				<MyFAB
+					icon={
+						showButtons ? (
+							<X color={theme.lightMode.white} size={20} />
+						) : (
+							<PathPilotLogoMini color={theme.lightMode.white} scale={0.8} />
+						)
+					}
+					color={theme.lightMode.primary}
+					className="absolute bottom-0 right-0"
+					onClick={handleShowButtons}
+				/>
+			</div>
 		</div>
 	);
 }
