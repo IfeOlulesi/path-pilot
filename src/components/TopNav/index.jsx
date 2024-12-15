80ec1460269cb0961b9459d4c672f60b68cf3948
Ife <ifeolulesi@gmail.com>
2024-12-15 01:27:26 +0100

    feat: Implemented UI layout v2

--- src/components/TopNav/index.jsx
+++ src/components/TopNav/index.jsx
@@ -1,9 +1,13 @@
+import PathPilotLogo from '@/assets/PathPilotLogo'
+
 export default function TopNav() {
 	return (
 		<div className="flex flex-row bg-white border-b-2 p-3 border-b-[#E4E7EC] justify-between">
-			<div></div>
+			<div className="flex place-content-center">
+        <PathPilotLogo color="#101928" />
+			</div>
 			<div className="text-sm flex 	flex-wrap content-center">Pick, select then visualize</div>
-			<div className="bg-[#02B549] py-2 px-4 rounded-md text-white text-sm">Visualize</div>
+			<div className="bg-[#1976D2] py-2 px-4 rounded-md text-white text-sm cursor-pointer">Visualize</div>
 		</div>
 	);
 }
