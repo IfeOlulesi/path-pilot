d2ce953d7eccdfe7bb7f777c4ca93a72629ba304
Ife <ifeolulesi@gmail.com>
2024-12-18 14:16:20 +0100

    feat: Added clear maze button

--- src/components/TopNav/index.jsx
+++ src/components/TopNav/index.jsx
@@ -1,12 +1,13 @@
 import PathPilotLogo from "@/assets/PathPilotLogo";
 import { useAppStore } from "@/store";
+import theme from "@/utils/theme";
 
 export default function TopNav() {
-	const { findShortestPath, startPos, endPos } = useAppStore();
+	const { initializeMaze, findShortestPath, startPos, endPos } = useAppStore();
 
 	function handleVisualize() {
+		// TODO: Add check for if visualization is currently running
 		// INFO: Gaurd clause to check if start and end pos are valid
-
 		if (startPos === null && endPos === null) {
 			alert("Choose starting and ending point");
 		} else if (startPos === null) {
@@ -14,24 +15,41 @@ export default function TopNav() {
 		} else if (endPos === null) {
 			alert("Choose ending point");
 		} else {
-			console.log("Visualizing...");
 			findShortestPath();
 		}
+	}
+
+	function handleClear() {
+		// TODO: Add check for if visualization is currently running
+		initializeMaze();
 	}
 
 	return (
 		<div className="flex flex-row bg-white border-b-2 p-3 border-b-[#E4E7EC] justify-between">
 			<div className="flex place-content-center">
-				<PathPilotLogo color="#101928" />
+				<PathPilotLogo color={theme.lightMode.primary} />
 			</div>
 			<div className="text-sm flex 	flex-wrap content-center">
 				Pick, select then visualize
 			</div>
-			<div
-				onClick={handleVisualize}
-				className="bg-[#1976D2] py-2 px-4 rounded-md text-white text-sm cursor-pointer"
-			>
-				Visualize
+			<div className="flex flex-row gap-2">
+				<div
+					onClick={handleClear}
+					className="content-center px-4 rounded-md text-sm cursor-pointer"
+          style={{
+            borderColor: theme.lightMode.primary,
+            color: theme.lightMode.primary,
+            borderWidth: '1px'
+          }}
+				>
+					Clear Maze
+				</div>
+				<div
+					onClick={handleVisualize}
+					className="bg-[#1976D2] py-2 px-4 rounded-md text-white text-sm cursor-pointer flex place-content-center"
+				>
+					Visualize
+				</div>
 			</div>
 		</div>
 	);
