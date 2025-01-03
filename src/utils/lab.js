bb3a417e22941b3ef2c4a3223e63fc021f316644
Ife <ifeolulesi@gmail.com>
2025-01-03 16:58:00 +0100

    wip: added dfs

--- src/utils/lab.js
+++ src/utils/lab.js
@@ -32,18 +32,8 @@ function createGraph(rows, cols) {
     type: "none",
   })));
 }
-const twoDGraph = createGraph(50, 50); // Create a n x m graph dynamically
 
-// Add diagonal walls
-for (let i = 0; i < twoDGraph.length - 1; i++) {
-  twoDGraph[i][i] = {
-    row: i,
-    col: i,
-    type: cells.wall.name
-  };
-}
-
-function bfs(start, end) {
+export function bfs(start, end, graph) {
   var queue = [start];
   var visitedCells = new Set();
   var predecessors = new Map();
@@ -63,7 +53,7 @@ function bfs(start, end) {
     };
 
     visitedCells.add(JSON.stringify(current));
-    const neighboursArr = getNeighbours(current);
+    const neighboursArr = getNeighbours(current, graph);
 
     // INFO: Explore the neighbors of current node
     for (let neighbour of neighboursArr) {
@@ -82,7 +72,49 @@ function bfs(start, end) {
   return false;
 }
 
-function getNeighbours(cell, maze = twoDGraph) {
+function dfs(start, end, graph) {
+  // queue to know where next to go
+  var counter = 0;
+  var queue = [start];
+  var visitedCells = new Set();
+  var predecessors = new Map();
+
+  while (queue.length > 0) {
+    let current = queue.shift();
+    if (!current) {
+      throw new Error("Current cell is undefined");
+    }
+
+    // INFO: Destination node found
+    if (JSON.stringify(current) === JSON.stringify(end)) {
+      console.log("Path found")
+      console.log("counter", counter);
+      return reconstructPath(predecessors, start, end);
+    };
+
+    visitedCells.add(JSON.stringify(current));
+    const neighboursArr = getNeighbours(current, graph);
+
+     // INFO: Explore the neighbors of current node
+     for (let neighbour of neighboursArr) {
+      const hasCellBeenVisited = visitedCells.has(JSON.stringify(neighbour));
+      if (!hasCellBeenVisited) {
+        queue.push(neighbour);
+        visitedCells.add(JSON.stringify(neighbour)); // wondering why this line tho
+        predecessors.set(JSON.stringify(neighbour), JSON.stringify(current)); // document the route to this node/cell
+        break; // Push only one neighbor to stack
+      }
+    }
+    counter++;
+  }
+
+  // no path found
+  console.log("No path found")
+  console.log("counter", counter);
+  return false;
+}
+
+function getNeighbours(cell, maze) {
   const [row, col] = cell;
   const neighbours = [];
   // find dimension of graph - max x, max y
@@ -96,22 +128,22 @@ function getNeighbours(cell, maze = twoDGraph) {
   const allNeighbours = [NC, WC, EC, SC];
 
   for (const neighbour in allNeighbours) {
-		const targetNeighbour = allNeighbours[neighbour];
+    const targetNeighbour = allNeighbours[neighbour];
 
-		// INFO: Gaurd against invalid indexes, ensure all indexes >= 0
-		if (targetNeighbour[0] >= 0 && targetNeighbour[1] >= 0) {
-			// INFO: Gaurd against out-of-range indexes i.e. index < elements in array
-			if (targetNeighbour[0] < rowMax && targetNeighbour[1] < colMax) {
-				const targetRow = targetNeighbour[0];
-				const targetCol = targetNeighbour[1];
+    // INFO: Gaurd against invalid indexes, ensure all indexes >= 0
+    if (targetNeighbour[0] >= 0 && targetNeighbour[1] >= 0) {
+      // INFO: Gaurd against out-of-range indexes i.e. index < elements in array
+      if (targetNeighbour[0] < rowMax && targetNeighbour[1] < colMax) {
+        const targetRow = targetNeighbour[0];
+        const targetCol = targetNeighbour[1];
 
-				if (maze[targetRow][targetCol].type !== cells.wall.name) {
-					// INFO: Gaurd against wall cells
-					neighbours.push(targetNeighbour);
-				}
-			}
-		}
-	}
+        if (maze[targetRow][targetCol].type !== cells.wall.name) {
+          // INFO: Gaurd against wall cells
+          neighbours.push(targetNeighbour);
+        }
+      }
+    }
+  }
   return neighbours;
 }
 
@@ -130,44 +162,52 @@ function reconstructPath(predecessors, start, end) {
 
 }
 
-const startingPos = [0, 1];
-const endingPos = [2,0];
-const shortestPath = bfs(startingPos, endingPos);
+(function main() {
+  const startingPos = [0, 1];
+  const endingPos = [2, 0];
 
-if (Array.isArray(shortestPath)) {
-  for (const node in shortestPath) {
-    const cords = shortestPath[node];
-    twoDGraph[cords[0]][cords[1]] = {
-      row: cords[0],
-      col: cords[1],
-      type: "path",
+  const twoDGraph = createGraph(10, 10); // Create a n x m graph dynamically
+  const shortestPath = dfs(startingPos, endingPos, twoDGraph);
+
+  // Add diagonal walls
+  for (let i = 0; i < twoDGraph.length - 1; i++) {
+    twoDGraph[i][i] = {
+      row: i,
+      col: i,
+      type: cells.wall.name
     };
   }
-}
 
-for (let row = 0; row < twoDGraph.length; row++) {
-  let rowString = '';
-  for (let col = 0; col < twoDGraph[row].length; col++) {
-    const cellType = twoDGraph[row][col].type
-    if (cellType === 'path') {
-      rowString += '⬛'
-    } else if (cellType === "begin") {
-      console.log('green')
-      rowString += '🟩'
-    } else if (cellType === 'finish') {
-      console.log('red')
-      rowString += '🟥'
-    } else if (cellType === "wall") {
-      rowString += "❎"
-    }
-    
-    else {
-      rowString += '⬜'
+  // update path types
+  if (Array.isArray(shortestPath)) {
+    for (const node in shortestPath) {
+      const cords = shortestPath[node];
+      twoDGraph[cords[0]][cords[1]] = {
+        row: cords[0],
+        col: cords[1],
+        type: "path",
+      };
     }
   }
-  console.log(rowString);
-}
-// console.log(twoDGraph);
 
+  // add cell colors
+  for (let row = 0; row < twoDGraph.length; row++) {
+    let rowString = '';
+    for (let col = 0; col < twoDGraph[row].length; col++) {
+      const cellType = twoDGraph[row][col].type
+      if (cellType === 'path') {
+        rowString += '⬛'
+      } else if (cellType === "begin") {
+        rowString += '🟩'
+      } else if (cellType === 'finish') {
+        rowString += '🟥'
+      } else if (cellType === "wall") {
+        rowString += "❎"
+      } else {
+        rowString += '⬜'
+      }
+    }
+    console.log(rowString);
+  }
 
-
+})()
